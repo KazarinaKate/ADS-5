aa5aef3e27ac2fa8210a3411ae3585b6873f6458
Казарина Катя <94719822+KazarinaKate@users.noreply.github.com>
2022-04-09 15:39:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,64 +3,93 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
-  TStack<char, 100> result;
-  TStack<char, 100> stack;
-  int currentNumber = 0;
-  for (int i = 0; i < inf.size(); i++) switch (inf[i]) {
-      case '+':
-      case '-':
-        if (stack.get() == '/' || stack.get() == '*')
-          result.push(stack.getAndPop());
-      case '*':
-      case '/':
-      case '(':
-        stack.push(inf[i]);
-        break;
-      case ')':
-        while (stack.get() != '(') result.push(stack.getAndPop());
-        stack.pop();
-        while (!stack.isEmpty()) {
-          if (stack.get() == '(' || stack.get() == '+' || stack.get() == '-')
-            break;
-          result.push(stack.getAndPop());
-        }
-      break;
-      default:
-        result.push(inf[i]);
-        break;
-    }
-  while (!stack.isEmpty()) result.push(stack.getAndPop());
-  std::string resString = "";
-  while (!result.isEmpty())
-    resString =
-        result.getAndPop() + (resString.size() == 0 ? "" : " " + resString);
-  printf("\n123: %s\n\n", resString.c_str());
-  return resString;
+int calculation(char op, int x, int y) {
+  switch (op) {
+  case '+': return (x + y);
+  case '-': return (y - x);
+  case '*': return (x * y);
+  case '/':
+    if (x != 0)
+      return y / x;
+  default: return 0;
+  }
 }
-     
-int eval(std::string pref) {
-  TStack<int, 100> stack;
-  for (int i = 0; i < pref.size(); i++) {
-    if (pref[i] >= '0') stack.push(pref[i] - '0');
-    switch (pref[i]) {
-      case '+':
-        stack.push(stack.getAndPop() + stack.getAndPop());
-        break;
-      case '-': {
-        int tmp = stack.getAndPop();
-        stack.push(stack.getAndPop() - tmp);
-        break;
-      }
-      case '*':
-        stack.push(stack.getAndPop() * stack.getAndPop());
-        break;
-      case '/': {
-        int tmp = stack.getAndPop();
-        stack.push(stack.getAndPop() / tmp);
-        break;
+int priority(char op) {
+  switch (op) {
+  case '(': return 0;
+  case ')': return 1;
+  case '-': return 2;
+  case '+': return 2;
+  case '*': return 3;
+  case '/': return 3;
+  case ' ': return 5;
+  default: return 4;
+  }
+}
+
+std::string infx2pstfx(std::string inf) {
+  std::string res;
+  char prob = ' ';
+  TStack <char, 100> stack;
+  for (int i = 0; i < inf.size(); i++) {
+    if (priority(inf[i]) == 4) {
+      res.push_back(inf[i]);
+      res.push_back(prob);
+    } else {
+      if (priority(inf[i]) == 0) {
+        stack.push(inf[i]);
+      } else if (stack.isEmpty()) {
+          stack.push(inf[i]);
+      } else if ((priority(inf[i]) > priority(stack.get()))) {
+          stack.push(inf[i]);
+      } else if (priority(inf[i]) == 1) {
+        while (priority(stack.get()) != 0) {
+          res.push_back(stack.get());
+          res.push_back(prob);
+          stack.pop();
+        }
+        stack.pop();
+      } else {
+        char a = priority(inf[i]);
+        char b = priority(stack.get());
+        while ((a <= b) && (!stack.isEmpty())) {
+          res.push_back(stack.get());
+          res.push_back(prob);
+          stack.pop();
+        }
+        stack.push(inf[i]);
       }
     }
   }
-  return stack.get();
+  while (!stack.isEmpty()) {
+    res.push_back(stack.get());
+    res.push_back(prob);
+    stack.pop();
+  }
+  for (int i = 0; i < res.size(); i++) {
+    if (res[res.size() - 1] == ' ')
+      res.erase(res.size() - 1);
+  }
+  return res;
+}
+     
+int eval(std::string pref) {
+  TStack <int, 100> res_stack;
+  int res = 0;
+  int x = 0;
+  int y = 0;
+  for (int i = 0; i < pref.size(); i++) {
+    if (priority(pref[i]) == 4) {
+      res_stack.push(pref[i] - '0');
+    } else if (priority(pref[i]) < 4) {
+      x = res_stack.get();
+      res_stack.pop();
+      y = res_stack.get();
+      res_stack.pop();
+      res_stack.push(calculation(pref[i], x, y));
+    }
+  }
+  res = res_stack.get();
+  return res;
+  return 0;
 }
